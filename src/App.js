aeb54e026adabd10115aaa38407df31058d93fa7
Uzias Rivera <uziasrivera@gmail.com>
2019-09-13 22:57:01 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        This is the template 🚀!
+      </p>
+    </div>
+  );
+}
+
+export default App;
